2eeda6b2c75d4b6888c2aa96f0258fa6da9f6122
Ssaveliy <saveliy-sokolnikov@mail.ru>
2022-04-30 22:03:45 +0300

    rewritten styles according to BEM methodology

--- src/App.tsx
+++ src/App.tsx
@@ -3,56 +3,39 @@ import "./App.css";
 const App = () => {
     return (
         <div className="wrapper">
-            <form className="search__bar">
+            <form className="search-bar">
                 <input
-                    className="search__bar__input"
+                    className="search-bar__input"
                     type="text"
                     placeholder="Moscow"
                 />
-                <button className="search__bar__btn iconBtn" type="submit">
-                    <img className="img-svg" src="icon/Shape.svg" alt="" />
+                <button className="search-bar__btn icon-btn">
+                    <img className="img-svg" src="icon/Shape.svg" alt="shape" />
                 </button>
             </form>
             <div className="info">
-                <div className="info__left info-block">
-                    <div className="info__left__screen active">
-                        <img src="" alt="" />
-                        <svg
-                            className="screen__HeartIcon iconBtn"
-                            width="24"
-                            height="25"
-                            viewBox="0 0 24 25"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                opacity="0.54"
-                                fill-rule="evenodd"
-                                clip-rule="evenodd"
-                                d="M17.5 1C15.0556 1 12.8556 2.7875 12 5.125C11.1444 2.7875 8.94444 1 6.5 1C3.44444 1 1 3.6125 1 7.1875C1 12 5.27778 16.125 12 23C18.7222 16.125 23 12 23 7.1875C23 3.6125 20.5556 1 17.5 1Z"
-                                stroke="black"
-                                stroke-width="2"
-                            />
-                        </svg>
-                        <div className="screen__temperature"></div>
-                        <div className="screen__CityName__TabNow cityName"></div>
+                <div className="left-info info-block">
+                    <div className="left-info__screen active now">
+                        <img src="" className="now__icon-heart" alt="heart" />
+                        <div className="now__temperature"></div>
+                        <div className="now__city-name city-name"></div>
                     </div>
-                    <div className="info__left__screen">
-                        <div className="screen__CityName__TabDetalis cityName"></div>
-                        <ul className="screen__detalis">
-                            <li className="det__temp"></li>
-                            <li className="det__like"></li>
-                            <li className="det__weather"></li>
-                            <li className="det__sunrise"></li>
-                            <li className="det__sunset"></li>
+                    <div className="left-info__screen detalis">
+                        <div className="detalis__city-name city-name"></div>
+                        <ul className="detalis__list">
+                            <li className="detalis__item detalis__item-temp"></li>
+                            <li className="detalis__item detalis__item-like"></li>
+                            <li className="detalis__item detalis__item-weather"></li>
+                            <li className="detalis__item detalis__item-sunrise"></li>
+                            <li className="detalis__item detalis__item-sunset"></li>
                         </ul>
                     </div>
-                    <div className="info__left__screen">
-                        <div className="info__left__screen-block">
-                            <div className="screen-block__CityName__foreCast cityName">
+                    <div className="left-info__screen forecast">
+                        <div className="forecast__wrapper">
+                            <div className="forecast__city-name city-name">
                                 Aktobe
                             </div>
-                            <div className="screen-block__ListWeeks"></div>
+                            <div className="forecast__list-weeks"></div>
                         </div>
                     </div>
                     <div className="buttons">
@@ -61,8 +44,8 @@ const App = () => {
                         <button className="btn tab">Forecast</button>
                     </div>
                 </div>
-                <div className="info__right info-block">
-                    <div className="info__right__locations">
+                <div className="right-info info-block">
+                    <div className="right-info__locations">
                         <div className="locations__name">Added Locations:</div>
                         <ul className="locations__list"></ul>
                     </div>
