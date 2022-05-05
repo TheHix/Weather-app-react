1261f58f79356f8cbbf8a7481f06c0e68e31955e
Ssaveliy <saveliy-sokolnikov@mail.ru>
2022-05-05 10:02:28 +0300

    functions for data conversion have been moved to the tools folder

--- src/components/tabs/tabForecast/ForecastItem.tsx
+++ src/components/tabs/tabForecast/ForecastItem.tsx
@@ -1,52 +1,13 @@
 import React from "react";
-const getTempInCelsius = (degreesInKelvin: number) => {
-    return Math.round(degreesInKelvin - 273);
-};
-// const getCompleteWeatherInformation = (weather:any) =>{
-//     let massWeathers = [];
-
-//     for (let i = 0; i < weather.length; i++){
-//         massWeathers.push(weather[i].main);
-//     }
-
-//     return massWeathers.join(', ');
-// }
-// const getTimeFromUNIX = (time_in_UNIX_timestamp:any) => {
-//     const usualTime = new Date(time_in_UNIX_timestamp * 1000);
-//     const hours = usualTime.getHours();
-//     const minutes = `0${usualTime.getMinutes()}`.slice(-2);
-//     return `${hours}:${minutes}`;
-// }
-const getDay = (dt_txt:string) => {
-    if (dt_txt.slice(8, 9) === "0") {
-        return dt_txt.slice(9, 10);
-    }
-
-    return dt_txt.slice(8, 10);
-};
-const getMonth = (dt_txt:string) => {
-    const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
-    return months[+dt_txt.slice(6, 7) - 1];
-};
-const getTime = (dt_txt: string) => {
-    return dt_txt.slice(11, 16);
-};
+import {
+    getDay,
+    getMonth,
+    getTempInCelsius,
+    getTime,
+} from "../../../tools/transform";
 interface IMain {
     temp: number;
-    feels_like:number;
+    feels_like: number;
 }
 interface IItem {
     main: IMain;
@@ -57,7 +18,7 @@ interface ForecastItemProps {
     item: IItem;
 }
 const ForecastItem: React.FC<ForecastItemProps> = ({ item }) => {
-    const {dt_txt, weather: cuurentWeather, main} = item;
+    const { dt_txt, weather: cuurentWeather, main } = item;
     const month = getMonth(dt_txt);
     const day = getDay(dt_txt);
     const time = getTime(dt_txt);
