135e95403827bdc8b742a1a99c99309794250665
Ssaveliy <saveliy-sokolnikov@mail.ru>
2022-05-06 11:12:28 +0300

    added display of the current weather on the details tab

--- src/components/tabs/tabDetalis/DetalisList.tsx
+++ src/components/tabs/tabDetalis/DetalisList.tsx
@@ -1,14 +1,45 @@
 import React from "react";
+import { useTypedSelector } from "../../../hooks/useTypedSelector";
+import {
+    getTempInCelsius,
+    getTimeFromUNIX,
+    getCompleteWeatherInformation,
+} from "../../../tools/transform";
+import Loader from "../../Loader";
 
 const DetalisList = () => {
+    const { currentWether, error, loading } = useTypedSelector(
+        store => store.currentWeather
+    );
+
+    if (loading) {
+        return <Loader />;
+    }
+    if (error) {
+        return <div>{error}</div>;
+    }
     return (
-        <ul className="detalis__list">
-            <li className="detalis__item detalis__item-temp"></li>
-            <li className="detalis__item detalis__item-like"></li>
-            <li className="detalis__item detalis__item-weather"></li>
-            <li className="detalis__item detalis__item-sunrise"></li>
-            <li className="detalis__item detalis__item-sunset"></li>
-        </ul>
+        <>
+            <div className="detalis__city-name city-name">
+                {currentWether!.name}
+            </div>
+            <ul className="detalis__list">
+                <li className="detalis__item detalis__item-like">
+                    Feels like:{" "}
+                    {getTempInCelsius(currentWether!.main.feels_like)}°
+                </li>
+                <li className="detalis__item detalis__item-weather">
+                    Weather:{" "}
+                    {getCompleteWeatherInformation(currentWether!.weather)}
+                </li>
+                <li className="detalis__item detalis__item-sunrise">
+                    Sunrise: {getTimeFromUNIX(currentWether!.sys.sunrise)}
+                </li>
+                <li className="detalis__item detalis__item-sunset">
+                    Sunset: {getTimeFromUNIX(currentWether!.sys.sunset)}
+                </li>
+            </ul>
+        </>
     );
 };
 
--- src/components/tabs/tabDetalis/TabDetalis.tsx
+++ src/components/tabs/tabDetalis/TabDetalis.tsx
@@ -4,7 +4,6 @@ import DetalisList from "./DetalisList";
 const TabDetalis = () => {
     return (
         <div className="left-info__screen detalis">
-            <div className="detalis__city-name city-name"></div>
             <DetalisList/>
         </div>
     );
